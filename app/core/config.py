865ed3c7f45cd2581d61d71840cc5a65b81541eb
Tim Danilov <volinadtim@gmail.com>
2025-12-31 16:57:03 +0300

    fix: add VERSION setting

--- app/core/config.py
+++ app/core/config.py
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
     
     # App
     PROJECT_NAME: str = "Beri Cosplay API"
+    VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     BACKEND_CORS_ORIGINS: List[str] = ["*"]
     
